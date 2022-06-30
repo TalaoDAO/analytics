f6e45648b421193430ef23a1ae36b009ded61728
achille1017 <97641902+achille1017@users.noreply.github.com>
2022-06-30 16:03:24 +0200

    Delete txTracker.py

--- txTracker.py
+++
@@ -1,8 +0,0 @@
-import model
-from pytezos import pytezos
-from decimal import Decimal
-print(model.eligible())
-def cashbackSender():
-    pytezos.using(key='edsk...', shell='babylonnet') \
-    .transaction(destination='tz1...', amount=Decimal('123.456')) \
-    .autofill().sign().inject()
