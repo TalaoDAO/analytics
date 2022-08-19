71ebc0d94c0ce195d05d0a085c65af128042af1a
achille1017 <achillemarseille@gmail.com>
2022-08-19 16:49:34 +0200

    verification amount to pay

--- txTracker/transactionsExecuter.py
+++ txTracker/transactionsExecuter.py
@@ -19,23 +19,19 @@ while True:
         if(len(payementToExecute[0])==36 or payementToExecute[0]==None):
             print(Decimal(payementToExecute[1]))
             sys.stdout.flush()
-            if(Decimal(payementToExecute[1])<0.001):
-                print("invalid amount "+ str(Decimal(payementToExecute[1])+str(Decimal(payementToExecute[1])<0.001)))
-                sys.stdout.flush()
-                model.setPayementDone(payementToExecute[3],"invalid amount to pay","date")
-            else:
-                hash=cashBackSender.cashbackSender(payementToExecute[1],payementToExecute[0])
+            
+            hash=cashBackSender.cashbackSender(payementToExecute[1]*1000000,payementToExecute[0])
+            time.sleep(10)
+            status=operationsVisualizer.getOperationStatus(hash)
+            print("status : "+str(status))
+            sys.stdout.flush()
+            while(status!="applied"):
                 time.sleep(10)
                 status=operationsVisualizer.getOperationStatus(hash)
                 print("status : "+str(status))
                 sys.stdout.flush()
-                while(status!="applied"):
-                    time.sleep(10)
-                    status=operationsVisualizer.getOperationStatus(hash)
-                    print("status : "+str(status))
-                    sys.stdout.flush()
-                #test balance
-                model.setPayementDone(payementToExecute[3],hash,"date")
+            #test balance
+            model.setPayementDone(payementToExecute[3],hash,"date")
     if(payementToExecute==None):
         time.sleep(3)
     
